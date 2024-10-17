e3e9f4ede8a46eb1365ee22315e91b8962d3371c
jimmyboy722 <jeckman722@gmail.com>
2024-10-16 21:18:43 -0400

    set up connection.js file for the database with notation

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// IMPORTING DOTENV PACKAGE AND SEQUELIZE LIBRARY
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+// CREATING A SEQUELIZE INSTANCE WITH A CONDITIONAL CONNECTION
+// IF DB_URL IS SET, USE IT, OTHERWISE USE A LOCAL CONNECTION AND THE PROCESSED ENVIRONMENT VARIABLES
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+      }
+    );
+
+// EXPORTING THE SEQUELIZE INSTANCE
+module.exports = sequelize;
