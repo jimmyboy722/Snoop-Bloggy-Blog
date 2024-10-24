e712eea0c19b06b8405e3b398fb6e0883f081cd9
jimmyboy722 <jeckman722@gmail.com>
2024-10-23 21:54:32 -0400

    added seed info in json files with notation

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,21 +3,21 @@ const sequelize = require("../config/connection");
 // IMPORTING THE USER AND POST MODELS
 const { User, Post } = require("../models");
 
-// IMPORTING JSON FILES CONTAINING USER AND POST DATA
-const userData = require("./userData.json");
-const postData = require("./postData.json");
+// IMPORTING JSON SEED FILES IN SAME DIRECTORY CONTAINING USER AND POST INFO
+const userInfo = require("./userInfo.json");
+const postInfo = require("./postInfo.json");
 
 const seedDb = async () => {
   // FUNCTION TO SEED THE DATABASE
   await sequelize.sync({ force: true }); // SYNCHRONIZES THE DATABASE, USING FORCE TO DROP AND RECREATE THE DATABASE
 
-  // USING BULKCREATE TO INSERT MULTIPLE USER RECORDS FROM THE USERDATA ARRAY
-  const users = await User.bulkCreate(userData, {
+  // USING BULKCREATE TO INSERT MULTIPLE USER RECORDS FROM THE USERinfo ARRAY
+  const users = await User.bulkCreate(userInfo, {
     individualHooks: true, // RUNS ANY DEFINED HOOKS FOR EACH USER, LIKE FOR PASSWORD ENCRYPTION
     returning: true, // RETURNS THE CREATED INSTANCES, ALLOWING ACCESS TO THEIR PROPERTIES
   });
   // CREATING POSTS
-  for (const post of postData) {
+  for (const post of postInfo) {
     // USING A LOOP TO ITERATE THROUGH THE POST DATA ARRAY AND CREATE A NEW ENTRY FOR EACH POST
     await Post.create({
       ...post, // SPREAD OPERATOR INCLUDES PROPERTIES FROM THE CURRENT POST OBJECT
