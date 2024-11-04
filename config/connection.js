f7612ee0d4e1c98c4ca6db745d86d8381afb0648
jimmyboy722 <jeckman722@gmail.com>
2024-11-03 20:43:02 -0500

    changed env variables for render deployment

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,8 @@ const sequelize = process.env.DB_URL
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
-        host: "localhost",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: "postgres",
       }
     );
